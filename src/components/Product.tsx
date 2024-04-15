903dae3f1a4370b26543cba3961f1d5e766b5b1b
RaghuNighthack8 <raghu.g@nighthack.in>
2024-04-15 09:15:38 +0530

    replaced inline styles with tailwind css

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -8,119 +8,39 @@ const Product = () => {
   const [firstItem, secondItem, thirdItem] = product.items;
 
   return (
-    <section className={`bg-background py-8`} id="product">
-      <div
-        className={`container max-w-5xl mx-auto m-8`}
-        style={{ position: 'relative' }}
-      >
-        <div
-          style={{
-            position: 'absolute',
-            left: '0',
-            top: '0',
-            bottom: '0',
-            width: '30px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <div
-            style={{
-              width: '2px',
-              height: '100%',
-              backgroundColor: 'transparent',
-              borderLeft: '2px dashed #ffffff',
-              position: 'absolute',
-              left: '50%',
-              transform: 'translateX(-50%)',
-            }}
-          ></div>
-          <div
-            style={{
-              width: '10px',
-              height: '10px',
-              backgroundColor: '#ffffff',
-              borderRadius: '50%',
-              marginBottom: '275px',
-              marginTop: '115px',
-            }}
-          ></div>
-          <div
-            style={{
-              width: '2px',
-              height: '100%',
-              backgroundColor: 'transparent',
-              borderLeft: '2px dashed #ffffff',
-              position: 'absolute',
-              left: '50%',
-              transform: 'translateX(-50%)',
-            }}
-          ></div>
-          <div
-            style={{
-              width: '10px',
-              height: '10px',
-              backgroundColor: '#ffffff',
-              borderRadius: '50%',
-              marginBottom: '315px',
-            }}
-          ></div>
-          <div
-            style={{
-              width: '2px',
-              height: '100%',
-              backgroundColor: 'transparent',
-              borderLeft: '2px dashed #ffffff',
-              position: 'absolute',
-              left: '50%',
-              transform: 'translateX(-50%)',
-            }}
-          ></div>
-          {/* Additional Pointer */}
-          <div
-            style={{
-              width: '10px',
-              height: '10px',
-              backgroundColor: '#ffffff',
-              borderRadius: '50%',
-              marginBottom: '80px',
-            }}
-          ></div>
-          <div
-            style={{
-              width: '2px',
-              height: '100%',
-              backgroundColor: 'transparent',
-              borderLeft: '2px dashed #ffffff',
-              position: 'absolute',
-              left: '50%',
-              transform: 'translateX(-50%)',
-            }}
-          ></div>
+    <section className="bg-background py-8" id="product">
+      <div className="container max-w-5xl mx-auto m-8 relative justify-center">
+        <div className="absolute left-0 top-0 bottom-0 w-px flex flex-col items-center">
+          <div className="w-px h-full bg-transparent border-l border-dashed border-white absolute left-1/2 transform -translate-x-1/2"></div>
+          <div className="w-5 h-10 bg-white rounded-full mt-20 mb-60"></div>
+          <div className="w-px h-275px bg-transparent border-l border-dashed border-white absolute left-1/2 transform -translate-x-1/2"></div>
+          <div className="w-5 h-10 bg-white rounded-full mt-5 mb-60"></div>
+          <div className="w-px h-100px bg-transparent border-l border-dashed border-white absolute left-1/2 transform -translate-x-1/2"></div>
+          <div className="w-5 h-10 bg-white rounded-full mt-15"></div>
+          <div className="w-px h-full bg-transparent border-l border-dashed border-white absolute left-1/2 transform -translate-x-1/2"></div>
         </div>
-        <div className={`flex flex-wrap`} style={{ paddingLeft: '50px' }}>
-          <div className={`w-5/6 sm:w-1/2 p-6 mt-20`}>
-            <h3 className={`text-3xl text-primary font-bold leading-none mb-3`}>
+        <div className="flex flex-wrap pl-20">
+          <div className="w-5/6 sm:w-1/2 p-6 mt-20">
+            <h3 className="text-3xl text-primary font-bold leading-none mb-3">
               {firstItem?.title}
             </h3>
-            <p className={`text-tertiary`}>{firstItem?.description}</p>
+            <p className="text-tertiary">{firstItem?.description}</p>
           </div>
         </div>
-        <div className={`flex flex-wrap`} style={{ paddingLeft: '50px' }}>
-          <div className={`w-full sm:w-1/2 p-6 mt-20`}>
-            <h3 className={`text-3xl text-primary font-bold leading-none mb-3`}>
+        <div className="flex flex-wrap pl-20">
+          <div className="w-full sm:w-1/2 p-6 mt-20">
+            <h3 className="text-3xl text-primary font-bold leading-none mb-3">
               {secondItem?.title}
             </h3>
-            <p className={`text-tertiary mb-8`}>{secondItem?.description}</p>
+            <p className="text-tertiary mb-8">{secondItem?.description}</p>
           </div>
         </div>
-        <div className={`flex flex-wrap`} style={{ paddingLeft: '50px' }}>
-          <div className={`w-5/6 sm:w-1/2 p-6 mt-20`}>
-            <h3 className={`text-3xl text-primary font-bold leading-none mb-3`}>
+        <div className="flex flex-wrap pl-20">
+          <div className="w-5/6 sm:w-1/2 p-6 mt-20">
+            <h3 className="text-3xl text-primary font-bold leading-none mb-3">
               {thirdItem?.title}
             </h3>
-            <p className={`text-tertiary`}>{thirdItem?.description}</p>
+            <p className="text-tertiary">{thirdItem?.description}</p>
           </div>
         </div>
       </div>

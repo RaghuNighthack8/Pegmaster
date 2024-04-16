23d9e1d55c110ff2993c8baea3eee11b4aeb47b2
RaghuNighthack8 <raghu.g@nighthack.in>
2024-04-16 13:35:36 +0530

    removed social media links

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -92,15 +92,6 @@ const About = () => {
             </a>
           </div>
         </div>
-
-        <div className="flex items-center mt-6">
-          <p className="text-xs lg:text-sm leading-none text-gray-900 dark:text-gray-50">
-            &copy; {new Date().getFullYear()} designed by{' '}
-            <a href="https://github.com/issaafalkattan" rel="nofollow">
-              Issaaf Kattan
-            </a>
-          </p>
-        </div>
       </div>
     </div>
   );
